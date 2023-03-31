bc9b6479f8b0bdcdc0b4cbee28fbf812149a81a8
Arno luyckx <A.luyckx@protonmail.com>
2023-03-31 23:58:32 +0200

    Made basic landing page.
    Co-authored-by: Emma Lou Who <Emmah-lou@users.noreply.github.com>

--- src/EldridiasElement.jsx
+++ src/EldridiasElement.jsx
@@ -1,10 +1,53 @@
 import { useState, useEffect, useRef } from "react";
+import { Router } from "react-router-dom";
 import { NavBar } from "./NavBar.jsx";
+
+const MainHome = () => {
+  return (
+    <main id="homeScreen">
+      <section className="hero">
+        <div id="hero-content-wrapper">
+          <h2>Eldridia's Element</h2>
+          <p>
+            Enter a world of magic and adventure, where you can collect and
+            trade powerful cards featuring epic heroes and fearsome monsters.
+          </p>
+          <button>Play Now!</button>
+        </div>
+      </section>
+      <section className="features">
+        <div className="feature">
+          <h3>Cards</h3>
+          <p>Discover new cards and build your ultimate deck.</p>
+          <button>View Cards</button>
+        </div>
+        <div className="feature">
+          <h3>Heroes</h3>
+          <p>Lead your team to victory with legendary heroes.</p>
+        </div>
+      </section>
+      <section className="about">
+        <h2>Eldridias Element</h2>
+        <p>
+          Welcome to the world of Eldridia, a land of magic and mystery, where
+          the elements of earth, air, water, fire, and magic reign supreme.
+          Here, you will embark on a journey through a fantastical realm filled
+          with danger and adventure. As a master of the elements, you will wield
+          incredible power and use your skills to overcome challenges and defeat
+          your foes. Will you be able to harness the power of Eldridia's
+          elements and emerge victorious? The fate of the world is in your
+          hands.
+        </p>
+      </section>
+    </main>
+  );
+};
 
 function EldridiasElement() {
   return (
     <div className="App">
       <NavBar />
+      <MainHome />
     </div>
   );
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,9 @@ function NavBar() {
   return (
     <nav>
       <ul>
-        <li>player</li>
+        <li>Cards/Hero's</li>
+        <li>Play Now!</li>
+        <li>Settings</li>
       </ul>
     </nav>
   );
