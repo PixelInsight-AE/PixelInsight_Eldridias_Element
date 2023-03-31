915480fc33e1e4cf386cf703c326985b77bab0db
Arno luyckx <A.luyckx@protonmail.com>
2023-03-31 23:23:39 +0200

    Made style-guide, some structure.
    Co-authored-by: Emma Lou Who <Emmah-lou@users.noreply.github.com>

---
+++ src/EldridiasElement.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, useRef } from "react";
+import { NavBar } from "./NavBar.jsx";
+
+function EldridiasElement() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default EldridiasElement;
---
+++ src/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>player</li>
+      </ul>
+    </nav>
+  );
+}
+
+export { NavBar };
