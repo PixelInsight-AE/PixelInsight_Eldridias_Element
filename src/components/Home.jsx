00aefb1a8f2122171a4e4cabee3f773d13bc67bc
Arno luyckx <A.luyckx@protonmail.com>
2023-04-02 02:53:57 +0200

    Home.jsx is cleanup up. All styling in it's module

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,6 +25,38 @@ const Hero = (props) => {
   );
 };
 
+const HeroDivider = () => {
+  return (
+    <section className="about">
+      <h2>Eldridias Element</h2>
+      <p>
+        Welcome to the world of Eldridia, a land of magic and mystery, where the
+        elements of earth, air, water, fire, and magic reign supreme. Here, you
+        will embark on a journey through a fantastical realm filled with danger
+        and adventure. As a master of the elements, you will wield incredible
+        power and use your skills to overcome challenges and defeat your foes.
+        Will you be able to harness the power of Eldridia's elements and emerge
+        victorious? The fate of the world is in your hands.
+      </p>
+    </section>
+  );
+};
+
+const SubHero = (props) => {
+  return (
+    <section className="subHero">
+      <div id="subHero-content-wrapper">
+        <h2>Collect and Trade</h2>
+        <p>
+          Collect and trade powerful cards featuring epic heroes and fearsome
+          monsters.
+        </p>
+        <button>View Cards</button>
+      </div>
+    </section>
+  );
+};
+
 const Party = (props) => {
   const [currentHero, setCurrentHero] = useState(
     props.party[props.party.length - 1]
@@ -42,11 +74,14 @@ const HeroCard = (props) => {
   return (
     <div key={props.name} className="hero">
       <div className="heroName">{props.name}</div>
-      <div className="heroHealth">{props.health}</div>
+      <div className="heroLevel">{props.level}</div>
       <div className="heroImg">
         <img src={props.imgUrl} alt="" />
       </div>
       <div className="heroType">{props.elementType}</div>
+      <div className="heroActions">
+        <button className="heroActionBtn">Equip Gear</button>
+      </div>
     </div>
   );
 };
@@ -56,33 +91,15 @@ const MainHome = (props) => {
     <main id="homeScreen">
       <Header />
       <Hero />
-      <section className="features">
-        <div className="feature">
-          <h3>Cards</h3>
-          <p>Discover new cards and build your ultimate deck.</p>
-          <button>View Cards</button>
-        </div>
-        <div className="feature">
-          <h3>Heroes</h3>
-          <p>Lead your team to victory with legendary heroes.</p>
-        </div>
-      </section>
-      <section className="about">
-        <h2>Eldridias Element</h2>
-        <p>
-          Welcome to the world of Eldridia, a land of magic and mystery, where
-          the elements of earth, air, water, fire, and magic reign supreme.
-          Here, you will embark on a journey through a fantastical realm filled
-          with danger and adventure. As a master of the elements, you will wield
-          incredible power and use your skills to overcome challenges and defeat
-          your foes. Will you be able to harness the power of Eldridia's
-          elements and emerge victorious? The fate of the world is in your
-          hands.
-        </p>
-      </section>
+      <HeroDivider />
+      <SubHero />
       <Party party={props.party} />
     </main>
   );
 };
 
 export { MainHome, Party, HeroCard };
+
+/**
+ * TODO: Also display element cards on the scrollable row
+ */
