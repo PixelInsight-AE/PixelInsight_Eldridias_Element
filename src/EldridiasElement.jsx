6fda5b329f829fb15f0b83c9e6a8228a17c9e49c
Arno luyckx <A.luyckx@protonmail.com>
2023-04-03 17:57:11 +0200

    Router for dashboard tabs is set up :) !
    Co-authored-by: Emma Lou Who <Emmah-lou@users.noreply.github.com>

--- src/EldridiasElement.jsx
+++ src/EldridiasElement.jsx
@@ -10,18 +10,91 @@ import {
 } from "./vanillaJsFiles/heros.js";
 import { MainHome } from "./components/Home.jsx";
 
-// ! For now I simply placed a back button to navigate back to the landing page.
-const Dashboard = () => {
+const OverView = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <Link to="/">
+      <h1>OverView</h1>
+      <Link to="/dashboard">
         <button>Back</button>
       </Link>
     </div>
   );
 };
 
+const Party = () => {
+  return (
+    <div>
+      <h1>Party</h1>
+      <Link to="/dashboard">
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
+
+const Deck = () => {
+  return (
+    <div>
+      <h1>Deck</h1>
+      <Link to="/dashboard">
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
+
+const Catalog = () => {
+  return (
+    <div>
+      <h1>Catalog</h1>
+      <Link to="/dashboard">
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop</h1>
+      <Link to="/dashboard">
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
+
+// ! For now I simply placed a back button to navigate back to the landing page.
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dashboard/overview">OverView</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/party">Party</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/deck">Deck</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/catalog">Catalog</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/shop">Shop</Link>
+          </li>
+        </ul>
+      </nav>
+      <Link to="/">
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
 function EldridiasElement() {
   const [party, setParty] = useState([
     controller,
@@ -38,6 +111,11 @@ function EldridiasElement() {
     <Routes>
       <Route path="/" element={<MainHome party={party} />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/overview" element={<OverView />} />
+      <Route path="/dashboard/party" element={<Party />} />
+      <Route path="/dashboard/deck" element={<Deck />} />
+      <Route path="/dashboard/catalog" element={<Catalog />} />
+      <Route path="/dashboard/shop" element={<Shop />} />
     </Routes>
   );
 }
