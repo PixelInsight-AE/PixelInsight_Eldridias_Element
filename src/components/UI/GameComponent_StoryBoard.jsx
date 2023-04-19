1d64cda4ddf64352b4d265949bad7ecfe72f4a0b
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-04-19 17:40:24 -0600

    did styling on story board
    Co-authored-by: Arno Luyckx <BlocksOnChainCode@users.noreply.github.com>

--- src/components/UI/GameComponent_StoryBoard.jsx
+++ src/components/UI/GameComponent_StoryBoard.jsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom";
-//import { story } from "../../vanillaJsFiles/storyboard";
-import { useState } from "react";
-import { NavBar } from "./NavBar";
-import "./GameComponent_StoryBoard.scss";
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import './GameComponent_StoryBoard.scss';
+import { TypeAnimation } from 'react-type-animation';
 
 const StoryBoard_Header = (props) => {
   const { story, currentStory } = props;
@@ -14,6 +13,106 @@ const StoryBoard_Header = (props) => {
         <button className="exit-button">Exit</button>
       </Link>
     </div>
+  );
+};
+
+const StoryBoard_AnimationScreen = (props) => {
+  return (
+    <div id="StoryBoard_AnimationScreen">
+      <h2>Animations</h2>
+    </div>
+  );
+};
+
+const StoryBoard_Content = ({ story, currentStory }) => {
+  return (
+    <div id="StoryBoard_Content">
+      {story[currentStory].playerText ? (
+        <div id="player-text">
+          <PlayerImg story={story} currentStory={currentStory} />
+        </div>
+      ) : (
+        <div id="npc-text">
+          <NpcImg story={story} currentStory={currentStory} />
+        </div>
+      )}
+      <StoryBoard_Dialog story={story} currentStory={currentStory} />
+    </div>
+  );
+};
+const StoryBoard_Dialog = (props) => {
+  const { story, currentStory } = props;
+  return (
+    <div id="StoryBoard_Text">
+      <p>
+        {story[currentStory].playerText}
+        {story[currentStory].npcText}
+      </p>
+    </div>
+  );
+};
+
+const StoryBoardNavigation = ({
+  story,
+  handleBack,
+  handleNext,
+  handleStart,
+  currentStory,
+}) => {
+  const startButton = (
+    <Link to={story[currentStory].path}>
+      <button onClick={handleNext}>Start</button>
+    </Link>
+  );
+  const buttons = (
+    <div>
+      <button onClick={handleBack}>Back</button>
+      <button onClick={handleStart}>Next</button>
+    </div>
+  );
+  return (
+    <div id="story-board-navigation">
+      {story[currentStory].pathIsValid ? startButton : buttons}
+    </div>
+  );
+};
+const StoryBoard_LootBox = (props) => {
+  return (
+    <div id="StoryBoard_LootBox">
+      <h2>LootBox</h2>
+    </div>
+  );
+};
+
+// ? Framer Motion Varient, (Object to store re-useable animation values) used in both PlayerImg and NpcImg
+const imgVariants = {
+  initial: { opacity: 0, scale: 0.75 },
+  animate: { opacity: 1, scale: 1 },
+  transition: { duration: 1, type: 'spring' },
+  whileHover: { scale: 1.2 },
+};
+
+const NpcImg = ({ story, currentStory }) => {
+  return (
+    <motion.img
+      src={story[currentStory].npcImg}
+      alt="blankfornow"
+      id="npc-img"
+      initial={{ opacity: 0, scale: 0.75 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1, type: 'spring' }}
+      whileHover={{ scale: 1.2 }}
+    />
+  );
+};
+const PlayerImg = ({ story, currentStory }) => {
+  return (
+    <motion.img
+      src={story[currentStory].playerImg}
+      alt="blankfornow"
+      id="player-img"
+      variants={imgVariants}
+    />
   );
 };
 
@@ -32,92 +131,21 @@ const StoryBoard = (props) => {
   const handleStart = () => {
     setCurrentStory(currentStory + 1);
   };
-
   return (
-    <>
-      <div
-        className="storyboard-background"
-        style={
-          {
-            // backgroundImage: `url(${story[currentStory].background})`,
-          }
-        }
-      >
-        <StoryBoard_Header story={story} currentStory={currentStory} />
+    <div id="StoryBoard">
+      <StoryBoard_Header story={story} currentStory={currentStory} />
+      <StoryBoard_AnimationScreen />
+      {story[currentStory].isReward ? <StoryBoard_LootBox /> : null}
 
-        <div id="storyBoard">
-          <div id="chapter-title">{story[currentStory].title}</div>
-          <div id="story-board-card">
-            {story[currentStory].playerText ? (
-              <div id="player-box">
-                <div id="player-box-image">
-                  <span>{story[currentStory].nameOfSpeaker}</span>
-                  {story[currentStory].imageIcon ? (
-                    <img
-                      className="player-image story-board-images"
-                      src={story[currentStory].imageIcon}
-                      alt="blankfornow"
-                    />
-                  ) : (
-                    <img
-                      className="player-image story-board-images"
-                      src={party[0].imgUrl}
-                      alt="blankfornow"
-                    />
-                  )}
-                </div>
-                <div id="player-box-text">{story[currentStory].playerText}</div>
-              </div>
-            ) : (
-              <div id="npc-box">
-                <div id="npc-box-text">{story[currentStory].npcText}</div>
-                <div id="npc-box-image">
-                  <span>{story[currentStory].nameOfSpeaker}</span>
-                  <img
-                    className="npc-image story-board-images"
-                    src={story[currentStory].imageIcon}
-                    alt="blankfornow"
-                  />
-                </div>
-              </div>
-            )}
-          </div>
-          <StoryBoardNavigation
-            story={story}
-            currentStory={currentStory}
-            handleBack={handleBack}
-            handleNext={handleNext}
-            handleStart={handleStart}
-          />
-        </div>
-      </div>
-    </>
-  );
-};
+      <StoryBoard_Content story={story} currentStory={currentStory} />
 
-const StoryBoardNavigation = ({
-  story,
-  handleBack,
-  handleNext,
-  handleStart,
-  currentStory,
-}) => {
-  const startButton = (
-    <Link to={story[currentStory].path}>
-      <button onClick={handleNext}>Start</button>
-    </Link>
-  );
-
-  const buttons = (
-    <div>
-      <button onClick={handleBack}>Back</button>
-      <button onClick={handleStart}>Next</button>
-    </div>
-  );
-
-  return (
-    <div id="storyboard-naviagation">
-      {story[currentStory].pathIsValid ? startButton : buttons}
+      <StoryBoardNavigation
+        story={story}
+        currentStory={currentStory}
+        handleBack={handleBack}
+        handleNext={handleNext}
+        handleStart={handleStart}
+      />
     </div>
   );
 };
