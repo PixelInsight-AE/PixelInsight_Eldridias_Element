709f07b6aeec750f6452f0043fd14d5cf52d2cde
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-19 05:50:20 -0600

    started new app
    Co-authored-by: BlocksOnChainCode <BlocksOnChainCode@users.noreply.github.com>

--- src/components/UI/Home/components/index.js
+++
@@ -1,9 +0,0 @@
-import { Header } from './Home_Header';
-import { HeroOne } from './Home_HeroOne';
-import { HeroTwo } from './Home_HeroTwo';
-import { HeroThree } from './Home_HeroThree';
-import { HeroFour } from './Home_HeroFour';
-import { HeroFive } from './Home_HeroFive';
-import { HeroSix } from './Home_HeroSix';
-
-export { Header, HeroOne, HeroTwo, HeroThree, HeroFour, HeroFive, HeroSix };
