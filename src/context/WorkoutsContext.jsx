382113e23ad113c384d2e66119c3a9913f20fb5f
Arno luyckx <A.luyckx@protonmail.com>
2023-06-07 17:52:45 +0200

    added some reference files

---
+++ src/context/WorkoutsContext.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { createContext, useReducer } from "react";
+
+export const WorkoutsContext = createContext();
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        ...state,
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        ...state,
+        workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        ...state,
+        workouts: state.workouts.filter(
+          (workout) => workout._id !== action.payload._id // ! this is the important part
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null,
+  });
+
+  return (
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
+  );
+};
---
+++ src/hooks/useAuthContext.js
@@ -0,0 +1,16 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuthContext must be used within an AuthContextProvider"
+    );
+  }
+
+  return context;
+};
+
+// ? React useContext documentation: https://reactjs.org/docs/hooks-reference.html#usecontext
